cfec72d82427c631df453bcc6d70f89968654916
Sergey Melnikov <74730932+pdasilem@users.noreply.github.com>
2023-02-26 17:20:31 +0400

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -2,7 +2,7 @@ public class Result {
 //    класс для вычисления. И проверка ошибки деления на ноль
     public static int calculate (int a, int b, String oper) {
         int res;
-        if (a < 1 || a > 10 || b < 1 || b > 10) throw new IllegalArgumentException("Цифры должны быть в пределах от 1 до 10. Выход! ");
+        if (a < 1 || a > 10 || b < 1 || b > 10) throw new IllegalArgumentException("Цифры должны быть в пределах от 1 до 10. Выход!");
         switch (oper) {
             case "+" : res = a + b; break;
             case "-" : res = a - b; break;
